ff5d291ba718af78f3974dba03d99b23711f8ed8
nancyreschka <nancy.reschka@gmail.com>
2020-01-10 15:44:08 -0800

    launching scripts generated

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/robond/workspace/catkin_ws; 
+source /home/robond/workspace/catkin_ws/devel/setup.bash;
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/robond/workspace/catkin_ws/src/map/nancys_world.world
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
---
+++ scripts/test_slam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/robond/workspace/catkin_ws; 
+source /home/robond/workspace/catkin_ws/devel/setup.bash;
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/robond/workspace/catkin_ws/src/map/nancys_world.world
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
