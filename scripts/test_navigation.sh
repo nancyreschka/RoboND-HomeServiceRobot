bf20ef431251f44793927af904e8af55348c9c1c
nancyreschka <nancy.reschka@gmail.com>
2020-01-16 20:33:59 -0800

    shell script for navigation

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -2,7 +2,8 @@
 
 cd /home/robond/workspace/catkin_ws; 
 source /home/robond/workspace/catkin_ws/devel/setup.bash;
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/robond/workspace/catkin_ws/src/map/nancys_world.world
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/robond/workspace/catkin_ws/src/RoboND-HomeServiceRobot/map/nancys_world.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/robond/workspace/catkin_ws/src/RoboND-HomeServiceRobot/map/nancys_map.yaml
 
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
