38f915d618b3f7723a3883b427efb5770aa2bf76
nancyreschka <nancy.reschka@gmail.com>
2020-01-16 20:33:41 -0800

    shell script for gmapping

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 cd /home/robond/workspace/catkin_ws; 
 source /home/robond/workspace/catkin_ws/devel/setup.bash;
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/robond/workspace/catkin_ws/src/map/nancys_world.world
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/robond/workspace/catkin_ws/src/RoboND-HomeServiceRobot/map/nancys_world.world
 
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
